335cd8a3ec62e5036bd1f2d4ebfb9ef7b31fc91d
heroOfThyme <p.vidako@gmail.com>
2025-05-28 03:06:47 -0400

    hero nav update

--- src/navigation.ts
+++ src/navigation.ts
@@ -111,8 +111,8 @@ export const headerData = {
       ],
     },
     {
-      text: 'Widgets',
-      href: '#',
+      text: 'Heroes',
+      href: '/heroes',
     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
